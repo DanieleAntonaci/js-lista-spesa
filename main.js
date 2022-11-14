8d3e5df87917afcb6e81da6d822477bcfc9992ee
DanieleAntonaci <danieleanto96@gmail.com>
2022-11-14 13:54:17 +0100

    Esercizio con console.log

---
+++ main.js
@@ -0,0 +1,19 @@
+let shoppingList = [
+    'Mele',
+    'Banane',
+    'Pasta',
+    'Carne',
+    'Latte'
+];
+let container = document.getElementById('container');
+
+// for (let i = 0; i < shoppingList.length; i++) {
+//     const element = shoppingList[i];
+//     console.log(shoppingList[i]);
+// }
+
+let i = 0;
+while (i < shoppingList.length) {
+    console.log(shoppingList[i]);
+    i++
+};
